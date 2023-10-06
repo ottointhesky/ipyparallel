cb5d2b73cbfc0fee80ef4e5ffc900ea2bfa505c4
Johannes Otepka <johannes.otepka@geo.tuwien.ac.at>
2023-10-06 17:54:28 +0200

    changes in ssh key handling

--- ipyparallel/tests/test_ssh.py
+++ ipyparallel/tests/test_ssh.py
@@ -14,19 +14,19 @@ from .test_cluster import test_to_from_dict  # noqa: F401
 
 
 @pytest.fixture(params=["SSH", "SSHProxy"])
-def ssh_config(ssh_key, request):
+def ssh_config(request):
     c = Config()
     c.Cluster.controller_ip = '0.0.0.0'
     c.Cluster.engine_launcher_class = request.param
     engine_set_cfg = c[f"{request.param}EngineSetLauncher"]
-    engine_set_cfg.ssh_args = [
-        "-o",
-        "UserKnownHostsFile=/dev/null",
-        "-o",
-        "StrictHostKeyChecking=no",
-        "-i",
-        ssh_key,
-    ]
+    engine_set_cfg.ssh_args = []
+    #    "-o",
+    #    "UserKnownHostsFile=/dev/null",
+    #    "-o",
+    #    "StrictHostKeyChecking=no",
+    #    "-i",
+    #    ssh_key,
+    #]
     engine_set_cfg.scp_args = list(engine_set_cfg.ssh_args)  # copy
     engine_set_cfg.remote_python = "/opt/conda/bin/python3"
     engine_set_cfg.remote_profile_dir = "/home/ciuser/.ipython/profile_default"
