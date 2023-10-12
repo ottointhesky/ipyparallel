03fbc202fef9b8106ffacc1470550f3dd14adc91
Johannes Otepka <johannes.otepka@geo.tuwien.ac.at>
2023-10-12 15:08:52 +0200

    linux command should run only 5 seconds

--- ipyparallel/tests/test_shellcmd.py
+++ ipyparallel/tests/test_shellcmd.py
@@ -35,7 +35,7 @@ def shellcmd_test_cmd():
     """returns a command that runs for 5 seconds"""
     test_command = {}
     test_command["Windows"] = 'ping -n 5 127.0.0.1' # "timeout 5"
-    test_command["Linux"] =  'ping -c 30 127.0.0.1'  # "sleep 5"   # # ping doesn't work/exist on the github docker image
+    test_command["Linux"] =  'ping -n 5 127.0.0.1'  # "sleep 5"   # # ping doesn't work/exist on the github docker image
     return test_command
 
 def test_all_shellcmds(setup_shellcmd_senders, shellcmd_test_cmd):
